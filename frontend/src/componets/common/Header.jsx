f4bcd3f6c29ce28f4dad7dfb495a00abfb2f80bb
Tushar <tusharpatel1093@gamil.com>
2026-03-05 15:30:01 +0530

    Call the Layout file in the Home page

--- frontend/src/componets/common/Header.jsx
+++ frontend/src/componets/common/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <h1>Header</h1>
+        </>
     )
 }
 
--- frontend/src/componets/pages/Home.jsx
+++ frontend/src/componets/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../common/Layout";
 
 const Home = () => {
     return (
-        <div>
+        <Layout>
             <h1>Home</h1>
-        </div>
+        </Layout>
     )
 }
 
